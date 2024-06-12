3003bdb0be81df72b1ca3ae2a883d40bed3f396b
DaviesAmedeus <amedeusedgar@gmail.com>
2024-06-13 00:28:25 +0300

    View data and Route Wildcards

---
+++ resources/views/jobs.blade.php
@@ -0,0 +1,9 @@
+<x-layout>
+    <x-slot:heading>Job Listings</x-slot:heading>
+    <ul>
+        @foreach ($jobs as $job )
+        <li><a href="jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline"><strong>{{ $job['title'] }}</strong> pays {{ $job['salary'] }}</a></li>
+        @endforeach
+    </ul>
+   
+</x-layout>
